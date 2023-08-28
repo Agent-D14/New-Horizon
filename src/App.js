84704b591f443505ea2762963f9afc345951b477
Agent-D14 <136282969+Agent-D14@users.noreply.github.com>
2023-08-28 18:55:41 -0500

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles
+        params={{
+          particles:{
+            number:{
+              value: 30, desnity:{ enable:true, value_area:900}
+            },
+            shape:{
+              type: "circle",
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }}
+      />
+      <Navbar/>
+      <Header/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../logo_org.png"
+
 //REACT FONTAWESOME IMPORT
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -9,7 +10,7 @@ const Navbar = () =>{
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="LOGO"></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <FontAwesomeIcon icon={faBars} className="hamburger"/>
+    <FontAwesomeIcon icon={faBars} className="hamburger" style={{outline:"none"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
