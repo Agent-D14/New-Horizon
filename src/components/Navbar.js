f45bf4f644fcd3f35aab17fc7f177b43fd83d8b3
Agent-D14 <136282969+Agent-D14@users.noreply.github.com>
2023-08-28 19:04:37 -0500

    Navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () =>{
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="LOGO"></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <FontAwesomeIcon icon={faBars} className="hamburger" style={{outline:"none"}}/>
+    <FontAwesomeIcon icon={faBars} className="hamburger" />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
